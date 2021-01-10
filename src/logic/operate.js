f9bbdfa4d888dd182d99f0f24e6109dce8cdb4fd
lamiasristy <lamiasristy@gmail.com>
2021-01-10 15:30:04 +0100

    Added functionalities inside the file operate.js.

---
+++ src/logic/operate.js
@@ -0,0 +1,37 @@
+import Big from 'big.js';
+
+const operator = (numberOne, numberTwo, operation) => {
+  let result;
+  try {
+    const num1 = Big(+numberOne);
+    const num2 = Big(+numberTwo);
+
+    if (operation === '+') {
+        result = Big(num1.plus(num2));
+    }
+
+    if (operation === '-') {
+        result = Big(num1.minus(num2));
+    }
+
+    if (operation === 'X') {
+        result = Big(num1.times(num2));
+    }
+
+    if (operation === '÷') {
+      result = Big(num1.div(num2));
+    }    
+
+    if (operation === '%') {
+      result = Big(num1.div(num2));
+    }    
+  } 
+  
+  catch (error) {
+    result = NaN;
+  }
+
+  return result.toPrecision();
+};
+
+export default operator;
