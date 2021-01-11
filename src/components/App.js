92e79177f6b2548dcff59664107b7cb704526748
lamiasristy <lamiasristy@gmail.com>
2021-01-11 11:40:00 +0100

    Added handleclick in the App.js and other functionalities.

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,32 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = buttonName => {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  };
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div>
+        <Display total={total} next={next} operation={operation} />
+        <ButtonPanel data={this.state} buttons={buttons} clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+
+}
 
 export default App;
